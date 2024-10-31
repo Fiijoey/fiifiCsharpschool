4121d01cfde26fe3442a9e4bd9483d3f2c555277
Fiijoey <kofimensah3730@gmail.com>
2024-10-31 17:17:48 +0000

    Done with prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("what is your grade percentage? : ");
+        int grades = int.Parse(Console.ReadLine());
+
+        if (grades < 60)
+        {
+            Console.WriteLine("You scored F");
+        }
+        else if (grades >= 60)
+        {
+            Console.WriteLine("You scored D");
+        }
+        else if (grades >= 70)
+        {
+            Console.WriteLine("You scored C");
+        }
+        else if (grades >= 80)
+        {
+            Console.WriteLine("You scored B");
+        }
+        else
+        {
+            Console.WriteLine("You scored A");
+        }
+
+
+        if (grades < 70)
+        {
+            Console.WriteLine("Unfortunately, you failed to pass. You can do better next time. ");
+        }
+        else
+        {
+            Console.WriteLine("Congratulations. You have passed.");
+        }
     }
 }
