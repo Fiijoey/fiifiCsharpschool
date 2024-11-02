94fdd2c9c5e3494e50f0f582a9956990369cf0b2
Fiijoey <kofimensah3730@gmail.com>
2024-11-02 22:41:27 +0000

    Done with the stretch for #3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,28 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random randNumbers = new Random();
-        int magicNumber = randNumbers.Next(1, 100);
-        int guessNumber = 0;
-
-        do
+        bool playAgain = true;
+        while (playAgain)
         {
-            Console.Write("What is your guess?: ");
-            guessNumber = int.Parse(Console.ReadLine());
 
-            if (magicNumber == guessNumber)
+            Random randNumbers = new Random();
+            int magicNumber = randNumbers.Next(1, 100);
+            int guessNumber = 0;
+            int guessCount = 0;
+
+            do
             {
-                Console.WriteLine("You guessed it!");
+                Console.Write("What is your guess?: ");
+                guessNumber = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (guessNumber < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guessNumber > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
             }
-            else if (guessNumber < magicNumber)
-            {
-                Console.WriteLine("Higher");
-            }
-            else
-            {
-                Console.WriteLine("Lower");
-            }
+            while (magicNumber != guessNumber);
+
+            Console.WriteLine($"Congratulations! You guessed the number in {guessCount} guesses.");
+
+            Console.WriteLine("Do you want to play again? (yes/no)");
+            string response = Console.ReadLine().ToLower();
+            playAgain = response == "yes";
         }
-        while (magicNumber != guessNumber);
+
     }
 }
