1514d827832f0e09ed0a938a56a7132a5d239cc2
Fiijoey <kofimensah3730@gmail.com>
2024-11-02 22:04:08 +0000

    loops

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randNumbers = new Random();
+        int magicNumber = randNumbers.Next(1, 100);
+        int guessNumber = 0;
+
+        do
+        {
+            Console.Write("What is your guess?: ");
+            guessNumber = int.Parse(Console.ReadLine());
+
+            if (magicNumber == guessNumber)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+        }
+        while (magicNumber != guessNumber);
     }
 }
