2f0a89ce60f6df5793904d74a2028adde8a10f9d
Fiijoey <kofimensah3730@gmail.com>
2024-11-14 22:39:33 +0000

    done

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -9,7 +9,8 @@ public class Program
         "What was the best part of my day?",
         "How did I see the hand of the Lord in my life today?",
         "What was the strongest emotion I felt today?",
-        "If I had one thing I could do over today, what would it be?"
+        "If I had one thing I could do over today, what would it be?",
+        "How did you serve others today?"
     };
 
     private static Journal journal = new Journal();
